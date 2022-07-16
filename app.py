a20c9fc6969e46814da2e30fc8356145605d8e9a
Slava0505 <31116179+Slava0505@users.noreply.github.com>
2022-07-17 00:28:30 +0700

    Dash app was added

--- app.py
+++ app.py
@@ -1,19 +1,45 @@
+import dash
+import dash_html_components as html
+import plotly.graph_objects as go
+import dash_core_components as dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
 import pandas as pd
-import streamlit as st
 
-LAT_LON = ['lat', 'lon']
+app = dash.Dash(__name__)
+
+
 
 
 df = pd.read_csv('data/covid_data_train.csv')
-df['lon'] = df['lng']
-
-st.title("Hi Anber! Welcome to Streamlit!")
-
-st.write("My first DataFrame")
 
 
-
-st.write(
-    df.iloc[:6]
+data=go.Scattermapbox(
+    lon=df['lng'],
+    lat=df['lat'],
+    text=df['name'],
+    mode='markers'
 )
-st.map(df)
+mapbox_access_token = 'your-free-token'
+
+layout = dict(margin=dict(l=0, t=0, r=0, b=0, pad=0),
+              mapbox_style="open-street-map")
+
+fig = go.Figure(data=data, layout=layout)
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Identified Geothermal Systems of the Western USA'),
+    html.Div(children='''
+        This data was provided by the USGS.
+    '''),
+
+    dcc.Graph(
+        id='example-map',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
